a98ec6689b869f1a2bd63fe3db80420f04a84f3b
Fabio Teodoro dos Santos <fabiots@ciandt.com>
2025-07-22 13:13:34 -0300

    chore: test correction

---
+++ jest.conf.js
@@ -0,0 +1,27 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!src/main.js'
+  ],
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'json',
+    'vue'
+  ],
+  transform: {
+    '^.+\\.vue$': '@vue/vue2-jest',
+    '^.+\\.jsx?$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  testMatch: [
+    '**/src/tests/**/*.spec.(js|jsx|ts|tsx)', // Ajuste para cobrir seus testes
+    '**/__tests__/*.(js|jsx|ts|tsx)'
+  ],
+  testEnvironmentOptions: {
+    url: 'http://localhost'
+  }
+}
--- src/gateways/gateway.js
+++ src/gateways/gateway.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-export async function chamarApi(){
-    try{
+export async function chamarApi() {
+    try {
         const resposta = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=30&offset=0");
         const pokemon = resposta.data.results;
         return pokemon;
     }
-    catch(erro){
-        console.error("Ocorreu um erro durante a chamada da API");
+    catch (erro) {
+        throw new Error("Ocorreu um erro durante a chamada da API");
     }
 }
