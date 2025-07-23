21a2a97f91b4d518c8a2407f62b83b315f0056ac
maysahellen <maysa.aragao@telefonica.com>
2025-07-23 15:25:03 -0300

    [BSBD-7706][BSBD-7710] test: gateway unit test

--- src/tests/gateways/gateway.spec.js
+++ src/tests/gateways/gateway.spec.js
@@ -1,34 +1,39 @@
 import axios from 'axios';
-import { chamarApi } from '../../gateways/gateway';
+import { callApi } from '../../gateways/gateway';
 
 jest.mock('axios');
 
 describe("when callApi is called", () => {
+
     describe('and the API is accessed', () => {
-        it('then return pokemon data', async () => {
-            const mockPokemons = Array.from({ length: 30 }, (_, index) => ({
+
+        let mockPokemons;
+        let pokemons;
+        
+        beforeEach( async () => {
+
+            mockPokemons = Array.from({ length: 30 }, (_, index) => ({ 
                 name: 'pokemon' + (index + 1),
                 url: 'https://pokeapi.co/api/v2/pokemon/' + (index + 1),
             }));
 
-            // Simula uma resposta bem-sucedida da API
-            axios.get.mockResolvedValue({ data: { results: mockPokemons } });
+            axios.get.mockResolvedValue({ data: { results: mockPokemons } }); 
 
-            const pokemons = await chamarApi();
-            expect(pokemons).toEqual(mockPokemons);
-            expect(axios.get).toHaveBeenCalledWith("https://pokeapi.co/api/v2/pokemon?limit=30&offset=0");
+            pokemons = await callApi(); 
         });
-    });
 
-    describe('and the API is not accessed', () => {
-        it('then it should show error message', async () => {
-            const mockErro = new Error('Ocorreu um erro durante a chamada da API');
-    
-            // Simula uma resposta com erro da API
-            axios.get.mockRejectedValue(mockErro);
-    
-            // Teste para rejeitar promessa e lançar erro
-            await expect(chamarApi()).rejects.toThrow('Ocorreu um erro durante a chamada da API');
+        it('then HTTPClient.get is called with correct params', async () => {
+            expect(axios.get).toHaveBeenCalledWith("https://pokeapi.co/api/v2/pokemon?limit=30&offset=0"); 
         });
-    });
+
+        it('then return pokemon data', async () => {
+
+            const expectedPokemons = Array.from({ length: 30 }, (_, index) => ({ 
+                name: 'pokemon' + (index + 1),
+                url: 'https://pokeapi.co/api/v2/pokemon/' + (index + 1),
+            }));
+
+            expect(pokemons).toEqual(expectedPokemons);
+        });
+    })
 });
