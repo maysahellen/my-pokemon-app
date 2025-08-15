90ded1ff022b193f9f414686ea95b86bdddd355c
maysahellen <maysa.aragao@telefonica.com>
2025-08-15 12:05:20 -0300

    [BSBD-7706][BSBD-7849] test: changes in the standard of unit tests

--- src/tests/unit/componentes/ListComponent.spec.js
+++ src/tests/unit/componentes/ListComponent.spec.js
@@ -12,39 +12,40 @@ const factory = (propsData) => shallowMount(ListComponent, {
 
 describe('Given ListComponent', () => {
 
+    let wrapper;
+    let mockProps;
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        mockProps = {
+            pokemons: []
+        }
+        wrapper = factory(mockProps);
+    });
+
+    afterEach(() => {
+        wrapper.destroy();
+    });
+
     describe('When the component is rendered', () => {
-
-        let wrapper;
-        let mockProps;
-        let pokemonsProp;
-        
-        beforeEach(() => {
-            jest.clearAllMocks();
-            mockProps = {
-                pokemons: []
-            }
-            wrapper = factory(mockProps);
-            pokemonsProp = wrapper.vm.$options.props.pokemons
-        });
-
-        afterEach(() => {
-            wrapper.destroy();
-        });
         
         it('Then the name is ListComponent', () => {
             expect(wrapper.vm.$options.name).toBe('ListComponent');
         });
 
         it('Then the type of the pokemons prop is array', () => {
-            expect(pokemonsProp.type).toBe(Array);
+            expect(wrapper.vm.$options.props.pokemons.type).toBe(Array);
         });
 
         it('Then the pokemons must be required', () => {
-            expect(pokemonsProp.required).toBe(true);
+            expect(wrapper.vm.$options.props.pokemons.required).toBe(true);
         });
 
-        it('Then the getId must return the id', () => {
-            expect(wrapper.vm.getId({ url: 'https://pokeapi.co/api/v2/pokemon/25/' })).toBe('25');
+        describe('And the function getId is called', () => {
+
+            it('Then the getId must return the id', () => {
+                expect(wrapper.vm.getId({ url: 'https://pokeapi.co/api/v2/pokemon/25/' })).toBe('25');
+            });
         });
     });
 });
--- src/tests/unit/componentes/LoadingComponent.spec.js
+++ src/tests/unit/componentes/LoadingComponent.spec.js
@@ -13,30 +13,7 @@ const factory = (propsData) => shallowMount(LoadingComponent, {
 
 describe('Given LoadingComponent', () => {
 
-    describe('When the component is rendered without props', () => {
-
-        let wrapper;
-
-        beforeEach(() => {
-            wrapper = factory();
-        });
-
-        afterEach(() => {
-            wrapper.destroy();
-        });
-
-        it('Then the default of the isLoading prop should be false', () => {
-            expect(wrapper.props().isLoading).toBe(false); 
-        });
-
-        it('Then the loading should not be rendered', () => {
-            expect(wrapper.find('.loading').exists()).toBe(false);
-        });
-    });
-
-    describe('When isLoading is false', () => {
-
-        let wrapper;
+            let wrapper;
         let isLoadingProp;
         let mockProps;
 
@@ -46,71 +23,29 @@ describe('Given LoadingComponent', () => {
                 isLoading: false
             }
             wrapper = factory(mockProps);
-            isLoadingProp = wrapper.vm.$options.props.isLoading;
         });
 
         afterEach(() => {
             wrapper.destroy();
         });
 
+    describe('When the component is rendered', () => {
+
+
         it('Then the name of the component is LoadingComponent', () => {
             expect(wrapper.vm.$options.name).toBe('LoadingComponent');
         });
 
         it('Then the type of the isLoading prop is Boolean', () => {
-            expect(isLoadingProp.type).toBe(Boolean);
+            expect(wrapper.vm.$options.props.isLoading.type).toBe(Boolean);
         });
 
         it('Then the isLoading should be false', () => {
-            expect(wrapper.props().isLoading).toBe(false);
+            expect(wrapper.vm.$options.props.isLoading.default).toBe(false);
         });
 
         it('Then there must be a loading image in the data', () => {
             expect(wrapper.vm.loadingImage).toBeDefined();
-        });
-
-        it('Then the loading should not be rendered', () => {
-            expect(wrapper.find('.loading').exists()).toBe(false);
-        });
-    });
-
-    describe('When isLoading is true', () => {
-
-        let wrapper;
-        let isLoadingProp;
-        let mockProps;
-
-        beforeEach(() => {
-            jest.clearAllMocks();
-            mockProps = {
-                isLoading: true
-            }
-            wrapper = factory(mockProps);
-            isLoadingProp = wrapper.vm.$options.props.isLoading;
-        });
-
-        afterEach(() => {
-            wrapper.destroy();
-        });
-
-        it('Then the name of the component is LoadingComponent', () => {
-            expect(wrapper.vm.$options.name).toBe('LoadingComponent');
-        });
-
-        it('Then the type of the isLoading prop is Boolean', () => {
-            expect(isLoadingProp.type).toBe(Boolean);
-        });
-
-        it('Then the isLoading should be true', () => {
-            expect(wrapper.props().isLoading).toBe(true);
-        });
-
-        it('Then there must be a loading image in the data', () => {
-            expect(wrapper.vm.loadingImage).toBeDefined();
-        });
-
-        it('Then the loading should be rendered', () => {
-            expect(wrapper.find('.loading').exists()).toBe(true);
         });
     });
 });
